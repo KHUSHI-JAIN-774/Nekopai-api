1f8f7a742a1e2b3ea5d116e14bc7ad5677e0987e
Neko Kun <123672103+NekoSenpai69@users.noreply.github.com>
2023-05-02 20:59:01 +0530

    Create animequotes.js
    Signed-off-by: Neko Kun <123672103+NekoSenpai69@users.noreply.github.com>

---
+++ SCRAPER/animequotes.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const quotesAnime = () => {
+    return new Promise((resolve, reject) => {
+        const page = Math.floor(Math.random() * 184)
+        axios.get('https://otakotaku.com/quote/feed/'+page)
+        .then(({ data }) => {
+            const $ = cheerio.load(data)
+            const hasil = []
+            $('div.kotodama-list').each(function(l, h) {
+                hasil.push({
+                    link: $(h).find('a').attr('href'),
+                    gambar: $(h).find('img').attr('data-src'),
+                    karakter: $(h).find('div.char-name').text().trim(),
+                    anime: $(h).find('div.anime-title').text().trim(),
+                    episode: $(h).find('div.meta').text(),
+                    up_at: $(h).find('small.meta').text(),
+                    quotes: $(h).find('div.quote').text().trim()
+                })
+            })
+            resolve(hasil)
+        }).catch(reject)
+    })
+}
+
+module.exports = {quotesAnime}
