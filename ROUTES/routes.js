2cb829d3a5db373d110e7060952bf37b8c67cabd
Devime69 <121723330+Devime69@users.noreply.github.com>
2023-05-03 13:39:40 +0000

    NekoSenpai

--- ROUTES/routes.js
+++ ROUTES/routes.js
@@ -4,7 +4,7 @@ const { truth, dare, couplepp, allcheck } = require("./../WEEBLOGIC/FUN/TDCC")
 const { pinterest, wikimedia, lyrics } = require("./../WEEBLOGIC/SEARCH/PGLW")
 const { ytsearch , github ,animeWallpaper } = require("./../WEEBLOGIC/SEARCH/YGAW")
 const { nsfwkona } = require("./../WEEBLOGIC/NSFW/Nsfw")
-
+const {glass,marvelstudio2,marvelstudio1,graffiti,captainamerica,pornhub,demon,magma,valentine,thor,addglow,grunge,metal3d,comic,golden,whitegold,sea3d,blackpink,black3d,transformer,harrypotter,paint,pinkgold,burger,neonstyle,slice,neonlight,thunder,matrix,thunder2,neon} = require("./../WEEBLOGIC/TEXTPRO/Textpro")
 
 // Section FUN :-
 
@@ -20,7 +20,9 @@ router.route("/allcheck").get(allcheck)
 // Section SEARCH:-
 
 router.route("/pinterest").get(pinterest)
+
 router.route("/wikimedia").get(wikimedia)
+
 router.route("/lyrics").get(lyrics)
 
 router.route("/github").get(github)
@@ -32,5 +34,78 @@ router.route("/yts").get(ytsearch)
 
 // section NSFW:-
 router.route("/nsfwkona").get(nsfwkona)
+
+
+// section TEXTPRO:-
+router.route("/glass").get(glass)
+
+router.route("/marvelstudio2").get(marvelstudio2)
+
+router.route("/marvelstudio1").get(marvelstudio1)
+
+router.route("/graffiti").get(graffiti)
+
+router.route("/captainamerica").get(captainamerica)
+
+router.route("/pornhub").get(pornhub)
+
+router.route("/demon").get(demon)
+
+router.route("/magma").get(magma)
+
+router.route("/valentine").get(valentine)
+
+router.route("/thor").get(thor)
+
+router.route("/metal3d").get(metal3d)
+
+router.route("/grunge").get(grunge)
+
+router.route("/addglow").get(addglow)
+
+router.route("/black3d").get(black3d)
+
+router.route("/blackpink").get(blackpink)
+
+router.route("/sea3d").get(sea3d)
+
+router.route("/whitegold").get(whitegold)
+
+router.route("/golden").get(golden)
+
+router.route("/comic").get(comic)
+
+router.route("/pinkgold").get(pinkgold)
+
+router.route("/paint").get(paint)
+
+router.route("/harrypotter").get(harrypotter)
+
+router.route("/transformer").get(transformer)
+
+router.route("/burger").get(burger)
+
+router.route("/slice").get(slice)
+
+router.route("/neonstyle").get(neonstyle)
+
+router.route("/burger").get(burger)
+
+router.route("/neonstyle").get(neonstyle)
+
+router.route("/neonlight").get(neonlight)
+
+router.route("/thunder").get(thunder)
+
+router.route("/matrix").get(matrix)
+
+router.route("/thunder2").get(thunder2)
+
+router.route("/neon").get(neon)
+
+
+
+
+
 
 module.exports = router
---
+++ WEEBLOGIC/TEXTPRO/Textpro.js
@@ -0,0 +1,342 @@
+const errorHandler = require("express-async-handler")
+const creator = "NekoSenpai69"
+const mumaker = require("mumaker");
+
+
+const glass = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-3d-style-glass-text-effect-online-1072.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+const neonstyle = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-gradient-neon-light-text-effect-online-1085.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+  const slice = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-light-glow-sliced-text-effect-online-1068.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const neonlight = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-glowing-neon-light-text-effect-online-free-1061.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const thunder = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/online-thunder-text-effect-generator-1031.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const matrix = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/matrix-style-text-effect-online-884.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const thunder2 = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-thunder-text-effect-online-881.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const neon = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/neon-text-effect-online-879.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const bookeh = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/bokeh-text-effect-876.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const addglow = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/free-advanced-glow-text-effect-873.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const grunge = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/grunge-metallic-3d-text-effect-online-1115.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const metal3d = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-3d-metallic-text-with-details-online-1108.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const comic = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-3d-comic-text-effects-online-1091.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const golden = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-realistic-golden-text-effect-on-red-sparkles-online-1082.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const whitegold = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/elegant-white-gold-3d-text-effect-online-free-1070.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const sea3d = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const blackpink = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-blackpink-logo-style-online-1001.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const black3d = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-a-metallic-text-effect-free-online-1041.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const transformer = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-a-transformer-text-effect-online-1035.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const harrypotter = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-harry-potter-text-effect-online-1025.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const paint = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-3d-multicolor-paint-text-effect-online-1114.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const pinkgold = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-pink-soft-gold-text-effect-online-1113.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const burger = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-burger-3d-text-effect-1111.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const demon = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-green-horror-style-text-effect-online-1036.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const magma = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-a-magma-hot-text-effect-online-1030.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const valentine = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-realistic-golden-text-effect-on-red-sparkles-online-1082.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const thor = errorHandler(async(req, res) => {
+let text = req.query.text
+  if(!text) return res.status(404).send("Query not found");
+mumaker.textpro("https://textpro.me/create-thor-logo-style-text-effect-online-1064.html", [
+   text
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const pornhub = errorHandler(async(req, res) => {
+let text1 = req.query.text1
+  let text2 = req.query.text2
+  if(!text1) return res.status(404).send("Query not found of Text1");
+if(!text2) return res.status(404).send("Query not found of Text2");
+
+mumaker.textpro("https://textpro.me/pornhub-style-logo-online-generator-free-977.html", [
+   text1,text2
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const captainamerica = errorHandler(async(req, res) => {
+let text1 = req.query.text1
+  let text2 = req.query.text2
+  if(!text1) return res.status(404).send("Query not found of Text1");
+if(!text2) return res.status(404).send("Query not found of Text2");
+
+mumaker.textpro("https://textpro.me/create-3d-avengers-logo-online-974.html", [
+   text1,text2
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const graffiti = errorHandler(async(req, res) => {
+let text1 = req.query.text1
+  let text2 = req.query.text2
+  if(!text1) return res.status(404).send("Query not found of Text1");
+if(!text2) return res.status(404).send("Query not found of Text2");
+
+mumaker.textpro("https://textpro.me/create-a-cool-graffiti-text-on-the-wall-1010.html", [
+   text1,text2
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const marvelstudio1 = errorHandler(async(req, res) => {
+let text1 = req.query.text1
+  let text2 = req.query.text2
+  if(!text1) return res.status(404).send("Query not found of Text1");
+if(!text2) return res.status(404).send("Query not found of Text2");
+
+mumaker.textpro("https://textpro.me/create-logo-style-marvel-studios-ver-metal-972.html", [
+   text1,text2
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+const marvelstudio2 = errorHandler(async(req, res) => {
+let text1 = req.query.text1
+  let text2 = req.query.text2
+  if(!text1) return res.status(404).send("Query not found of Text1");
+if(!text2) return res.status(404).send("Query not found of Text2");
+
+mumaker.textpro("https://textpro.me/create-logo-style-marvel-studios-online-971.html", [
+   text1,text2
+ ])
+ .then((data) => res.json({"creator":"NekoSenapi69","url": data}))
+          })
+
+
+
+
+module.exports = {glass,marvelstudio2,marvelstudio1,graffiti,captainamerica,pornhub,demon,magma,valentine,thor,addglow,grunge,metal3d,comic,golden,whitegold,sea3d,blackpink,black3d,transformer,harrypotter,paint,pinkgold,burger,neonstyle,slice,neonlight,thunder,matrix,thunder2,neon}
